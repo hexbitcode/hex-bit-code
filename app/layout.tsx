efbd213db5e821d1d727af52a97158fea81e8eab
dileepkushwaha48 <dileepkushwaha2222@gmail.com>
2025-12-23 17:51:11 +0545

    modified in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-<<<<<<< HEAD
 import Navbar from "@/components/layout/Navbar"
-
-=======
-import Navbar from "./components/layout/Navbar"
-import Footer from "./components/layout/Footer"
->>>>>>> fe3af8b (Add Footer component and update RootLayout)
+import Footer from "@/components/layout/Footer"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
